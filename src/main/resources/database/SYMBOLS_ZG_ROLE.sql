367497f970734f4047f19a13a61f581beaa4dc43
song <247322694@qq.com>
2019-12-25 19:46:13 +0800

    111

---
+++ src/main/resources/database/SYMBOLS_ZG_ROLE.sql
@@ -0,0 +1,40 @@
+create table ZG_ROLE
+(
+    ID          NUMBER not null
+        constraint ROLE_PK
+            primary key,
+    NAME        VARCHAR2(50),
+    ROLE_LEVEL  NUMBER,
+    DESCRIPTION VARCHAR2(50)
+)
+/
+
+
+create table ZG_USER
+(
+    ID            NUMBER not null
+        constraint USER_PK
+            primary key,
+    CNNAME        VARCHAR2(50),
+    USERNAME      VARCHAR2(50),
+    PASSWORD      VARCHAR2(50),
+    EMAIL         VARCHAR2(50),
+    TELEPHONE     VARCHAR2(50),
+    MOBILE_PHONE  VARCHAR2(50),
+    WECHAT_ID     VARCHAR2(50),
+    SKILL         VARCHAR2(50),
+    DEPARTMENT_ID NUMBER,
+    LOGIN_COUNT   NUMBER
+)
+/
+
+INSERT INTO SYMBOLS.ZG_USER (ID, CNNAME, USERNAME, PASSWORD, EMAIL, TELEPHONE, MOBILE_PHONE, WECHAT_ID, SKILL, DEPARTMENT_ID, LOGIN_COUNT) VALUES (1, '程柯', 'admin', 'admin', null, null, null, null, null, null, null);
+create table ZG_USER_ROLE
+(
+    USER_ID NUMBER not null,
+    ROLE_ID NUMBER not null,
+    constraint USER_ROLE_PK
+        primary key (USER_ID, ROLE_ID)
+)
+/
+
