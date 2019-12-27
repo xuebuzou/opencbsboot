18bcfa3665d38babab7db5f384006608c40ea6cf
xuebuzou <247322694@qq.com>
2019-12-27 15:25:44 +0800

    commit at bocd

--- src/main/java/cn/com/bocd/opencbsboot/config/DruidStatFilter.java
+++ src/main/java/cn/com/bocd/opencbsboot/config/DruidStatFilter.java
@@ -1,19 +1,13 @@
 package cn.com.bocd.opencbsboot.config;
 
 import javax.servlet.annotation.WebFilter;
-
 import javax.servlet.annotation.WebInitParam;
-
 import com.alibaba.druid.support.http.WebStatFilter;
 
 @WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*",
-
         initParams = {
-
-                @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
-
+                @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")
         })
-
 public class DruidStatFilter extends WebStatFilter {
 
 }
