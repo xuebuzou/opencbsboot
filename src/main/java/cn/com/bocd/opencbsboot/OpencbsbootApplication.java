df148ed5f0928a9742db4fdb640fd8a38864a046
xuebuzou <247322694@qq.com>
2019-12-27 17:24:50 +0800

    commit at bocd

--- src/main/java/cn/com/bocd/opencbsboot/OpencbsbootApplication.java
+++ src/main/java/cn/com/bocd/opencbsboot/OpencbsbootApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableAsync
 public class OpencbsbootApplication {
     private static final Logger logger = Logger.getLogger(OpencbsbootApplication.class);
     public static void main(String[] args) {
