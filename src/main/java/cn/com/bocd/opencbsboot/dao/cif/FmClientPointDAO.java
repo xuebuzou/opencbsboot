7fe627d9b256336e253e7e1f16da5e6a4a25f2e4
xuebuzou <247322694@qq.com>
2019-12-28 11:51:48 +0800

    commit at bocd

--- src/main/java/cn/com/bocd/opencbsboot/dao/cif/FmClientPointDAO.java
+++ src/main/java/cn/com/bocd/opencbsboot/dao/cif/FmClientPointDAO.java
@@ -3,11 +3,6 @@ package cn.com.bocd.opencbsboot.dao.cif;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-/**
- * @创建人 chengke
- * @创建时间 2019/12/3
- * @描述 FM_CLIENT_POINT表的DAO
- */
 @Mapper
 public interface FmClientPointDAO {
     int selectClientTotalPointByClientNo(@Param("clientNo") String clientNo);
