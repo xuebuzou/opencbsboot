25ba0a91c9be930c2722317974857ac5da1109de
song <247322694@qq.com>
2019-12-30 19:12:47 +0800

    commit at home

--- src/main/resources/sql/ZG_ZG_ROLE.sql
+++ src/main/resources/sql/ZG_ZG_ROLE.sql
@@ -1,8 +1,12 @@
-CREATE TABLE ZG.ZG_ROLE
+create table ZG_ROLE
 (
-    ID NUMBER(*) PRIMARY KEY NOT NULL,
-    NAME VARCHAR2(50),
-    ROLE_LEVEL NUMBER(*),
+    ID          NUMBER not null
+        primary key,
+    NAME        VARCHAR2(50),
+    ROLE_LEVEL  NUMBER,
     DESCRIPTION VARCHAR2(50)
-);
+)
+/
+
 INSERT INTO ZG.ZG_ROLE (ID, NAME, ROLE_LEVEL, DESCRIPTION) VALUES (1, 'admin', null, '系统管理员');
+INSERT INTO ZG.ZG_ROLE (ID, NAME, ROLE_LEVEL, DESCRIPTION) VALUES (2, 'accounting', null, '会计岗');
